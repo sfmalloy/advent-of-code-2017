e316e855d72b8b0ca075f8d6ec0d05258eae2f48
Sean Malloy <techfan321@gmail.com>
2024-12-07 21:27:33 -0500

    day 17

--- solutions/d17.py
+++ solutions/d17.py
@@ -1,18 +1,33 @@
 from lib import advent
 from io import TextIOWrapper
-from collections import deque, defaultdict
 
 
 @advent.parser(17)
 def parse(file: TextIOWrapper):
-    return [line.strip() for line in file.readlines()]
+    return int(file.read())
 
 
 @advent.solver(17, part=1)
-def solve1(ipt):
-    return 0
+def solve1(ipt: int) -> int:
+    return value_after(ipt, 2017, False)
 
 
 @advent.solver(17, part=2)
-def solve2(ipt):
-    return 0
+def solve2(ipt: int) -> int:
+    return value_after(ipt, 50_000_000, True)
+
+
+def value_after(ipt: int, limit: int, part2: bool):
+    i = 0
+    for l in range(1, limit+1):
+        i += (ipt + 1)
+        i %= l
+    goal = 0 if part2 else i
+    while True:
+        i -= (1 + ipt)
+        i %= l
+        if i < goal:
+            goal -= 1
+        l -= 1
+        if goal == i:
+            return l
